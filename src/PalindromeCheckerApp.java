11392cb82e056f47f5d74af79673d07b9d8ea959
vedhangakrishnareddykusuluru <vk4872@srmist.edu.in>
2026-02-25 14:35:53 +0530

    UC1:Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,7 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcome to the palindrome checker management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
